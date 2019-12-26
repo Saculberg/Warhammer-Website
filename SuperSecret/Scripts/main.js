918ed847862bcc808f890bb7d11ec8913fa6746b
Lucas Berg <lucas.t.berg@live.se>
2019-12-26 18:41:00 +0100

    waow

---
+++ SuperSecret/Scripts/main.js
@@ -0,0 +1,34 @@
+﻿var $audio1 = $('#myAudio1');
+var button1 = document.getElementById("button1");
+
+$('#input1').on('change', function(e) {
+    
+    var target = e.currentTarget;
+    var file = target.files[0];
+    var reader = new FileReader();
+  
+    console.log($audio1[0]);
+    if (target.files && file) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $audio1.attr('src', e.target.result);
+            $audio1.play();
+            
+        }
+        reader.readAsDataURL(file);
+    }
+});
+
+$("#button1").click(function () {
+    $("#input1").trigger('click');
+    this.innerHTML = prompt("Please enter a name", "default");
+    //setTimeout(function(){ 
+    //    this.innerHTML = prompt("Please enter a name", "default");
+    //}, 1);
+});
+
+var slide1 = document.getElementById('slider1')
+
+slide1.oninput = function() {
+    document.getElementById('myAudio1').volume = this.value; 
+}
