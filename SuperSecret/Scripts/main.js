a72ae846ed615eadce20c9ab5545eac9592b7b1b
Lucas Berg <lucas.t.berg@live.se>
2019-12-27 01:02:58 +0100

    waow

--- SuperSecret/Scripts/main.js
+++ SuperSecret/Scripts/main.js
@@ -1,14 +1,14 @@
 ﻿function download(filename, text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
 
-  element.style.display = 'none';
-  document.body.appendChild(element);
+    element.style.display = 'none';
+    document.body.appendChild(element);
 
-  element.click();
+    element.click();
 
-  document.body.removeChild(element);
+    document.body.removeChild(element);
 }
 
 var $audio1 = $('#myAudio1');
@@ -300,3 +300,138 @@ $("#button8").click(function () {
 document.getElementById('slider8').oninput = function() {
     document.getElementById('myAudio8').volume = this.value; 
 }
+
+function getCurrentValue(){
+    return [
+            document.getElementById('slider1').value,
+            document.getElementById('slider2').value,
+            document.getElementById('slider3').value,
+            document.getElementById('slider4').value,
+            document.getElementById('slider5').value,
+            document.getElementById('slider6').value,
+            document.getElementById('slider7').value,
+            document.getElementById('slider8').value
+        ];
+}
+
+function linearProgress(start, end, progress){
+    amt = progress / 100.0;
+    return (1-amt)*start+amt*end
+}
+
+function loadPrefab(target, start, progress){
+    
+    for(var i = 0; i < target.length; i++){
+        document.getElementById("slider" + (i + 1)).value = linearProgress(start[i], target[i], progress)
+        document.getElementById("myAudio" + (i + 1)).volume = linearProgress(start[i], target[i], progress)
+	}
+    if(progress < 100)
+        setTimeout(() => loadPrefab(target, start, progress + 1), 5);
+}
+
+var prefabIsSet = [
+    false,
+    false,
+    false,
+    false
+];
+var prefabs = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]
+];
+
+function prefabCall(index){
+
+    if(prefabIsSet[index])
+        loadPrefab(prefabs[index], getCurrentValue(), 0);
+    else{
+        prefabIsSet[index] = true;
+
+        prefabs[index] = getCurrentValue();
+
+        document.getElementById("prefabButton" + (index + 1)).innerHTML = "<div class=\"buttonText\">" + prompt("Please enter a name", "default") + "</div>";
+	}
+}
+
+function load(){
+    $("#inputLoad").trigger('click');
+}
+$('#inputLoad').on('change', function(e) {
+    
+    var reader = new FileReader();
+  
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            setLoad(e.target.result);
+        }
+        reader.readAsText(e.currentTarget.files[0]);
+    }
+});
+
+function setLoad(str){
+    var data = str.split("|");
+
+    var prefabData = data[0].split(",");
+
+    for(var i = 0; i < 4; i++){
+        for(var j = 0; j < 8; j++){
+            prefabs[i][j] = parseFloat(prefabData[i * 8 + j]);
+		}
+	}
+
+    var prefabIsSetData = data[1].split(",");
+
+    for(var i = 0; i < prefabIsSetData.length; i++){
+        prefabIsSet[i] = "True" == prefabIsSetData[i];
+	}
+
+    var prefabNames = data[2].split(",");
+
+    for(var ie = 0; ie < 4; ie++){
+        (document.getElementById("prefabButton" + (ie + 1))).innerHTML = "<div class=\"buttonText\">" + prefabNames[ie] + "</div>";
+	}
+
+    var buttonNames = data[3].split(",");
+
+    for(var i = 0; i < 8; i++){
+        (document.getElementById("button" + (i + 1))).innerHTML = "<div class=\"buttonText\">" + buttonNames[i] + "</div>";
+	}
+
+    var urls = data[4].split(",");
+
+    for(var e = 0; e < urls.length; e++){
+        (document.getElementById("myAudio" + (e + 1))).src = (urls[e]);
+	}
+}
+
+function save(){
+    var savs = "";
+    //savs += prefabs;
+    //savs += "|";
+    //savs += prefabIsSet;
+    //savs += "|";
+    //for(var ie = 0; ie < 4; ie++){
+    //    savs += (document.getElementById("prefabButton" + (ie + 1))).innerHTML.substring(24).split("<")[0];
+    //    if(ie < 3)
+    //        savs += ",";
+	//}
+    //savs += "|";
+    //for(var i = 0; i < 8; i++){
+    //    savs += (document.getElementById("button" + (i + 1))).innerHTML.substring(24).split("<")[0];
+    //    if(i < 7)
+    //        savs += ",";
+	//}
+    //savs += "|";
+    for(var h = 0; h < 8; h++){
+        savs += (document.getElementById("myAudio" + (h + 1))).src;
+        if(h < 7)
+            savs += ",";
+	}
+
+    download("default.txt", savs);
+}
+
+//download("def.txt", "hejkqwjwhffui aoduiadjiafihaoiah fioahiofuasiofuaoifuaofiuasofiuaiouf iaouafio uaiouaio uaofi uaio fuaoifuaiofuaiofuaio a fiuaoi fua foiuafio uafoi aufoiu afiuaf oiafu aofiu afoiufa oiufaofuiaofiu afoiuafofiua ofuiaofiaua foiufaoiufaouifaofiuafouiafoiufaof uiaofiauf oafu afoi afoiuafioafuafoiuafiou afiouaf");
